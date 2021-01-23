f6ad0adb82127f4b18c61c135a498e283aee42ac
cfmbido <cfmbido@gmail.com>
2021-01-23 17:06:10 +0700

    added login,register,landing,order

--- my-app/pages/ProfilePage.js
+++ my-app/pages/ProfilePage.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableHighlight } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ProfilePage() {
+  const dataProfile = {
+    name: 'Bambang',
+
+  }
+
   return (
+    <LinearGradient
+              // Background Linear Gradient
+              colors={['#008bb5','#48bcae']}
+              style={{height: '100%'}}
+          >
     <View style={styles.container}>
-      <Text>ProfilePage</Text>
+      <View style={{flex: 0.8, flexDirection: 'row'}}>
+        <Image source={{ uri: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"}} style={styles.profileImg} />
+        <Text>{dataProfile.name}</Text>
+        <Text>{dataProfile.name}</Text>
+      </View>
+      
+      <View>
+        <TouchableHighlight style={styles.button} onPress={e => {console.log('masuk')}}>
+          <Text>LOGOUT</Text>
+        </TouchableHighlight>
+      </View>
     </View>
+    </LinearGradient>
   );
 }
 
@@ -15,4 +37,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  profileImg: {
+    width: 100,
+    height: 100,
+    borderRadius: 150 / 2,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: "red"
+  },
+  button: {
+    alignItems: "center",
+    marginBottom: '2%',
+    padding: 10,
+    borderWidth: 3,
+    borderColor: 'black',
+    backgroundColor: 'rgba(255, 0, 0, 0.8)',
+    borderRadius: 30,
+    width: '30%'
+  }
 });
